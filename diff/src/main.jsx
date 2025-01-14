597677530d839c5415631ac06377fefd13cfcd46
mitch <mitchellbermin10@gmail.com>
2025-01-13 22:02:50 -0500

    Arreglo rutas

--- diff/src/main.jsx
+++ diff/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Index from './index.jsx'
+import Index from './Index.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
